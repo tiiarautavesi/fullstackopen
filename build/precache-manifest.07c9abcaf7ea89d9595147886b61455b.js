f8d058527052177e0654baf4cde85fa45c90ed08
Tiia <tiia.rautavesi@siili.com>
2019-05-16 17:40:46 +0300

    update styling and structure

---
+++ build/precache-manifest.07c9abcaf7ea89d9595147886b61455b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a133fc27a3eacb439a24",
+    "url": "/static/js/main.a133fc27.chunk.js"
+  },
+  {
+    "revision": "5fac788e1c70b42664b5",
+    "url": "/static/js/1.5fac788e.chunk.js"
+  },
+  {
+    "revision": "a133fc27a3eacb439a24",
+    "url": "/static/css/main.92cd2490.chunk.css"
+  },
+  {
+    "revision": "cfa15229b69336d822c6424af27fa4d6",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74ccb4d65d9f8dfefe4c51ad732cc6fd.js"
+  "/precache-manifest.07c9abcaf7ea89d9595147886b61455b.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
